e6c257efe362ad41b2a863808088a0312309ccf1
Arjun Thippeswamy <ht.arjun@outlook.com>
2019-02-13 23:26:42 -0600

    Setup reducers

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import combineReducers from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(combineReducers)
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+)
